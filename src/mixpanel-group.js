8a5b5c849541fc5de57eba61a28359ce30cfccd7
Chris Knuteson <cknuteson@knowland.com>
2025-03-21 19:37:06 -0400

    add api_host per endpoint

--- src/mixpanel-group.js
+++ src/mixpanel-group.js
@@ -1,20 +1,24 @@
 /* eslint camelcase: "off" */
-import { addOptOutCheckMixpanelGroup } from './gdpr-utils';
-import { apiActions } from './api-actions';
-import { _ } from './utils';
+import { addOptOutCheckMixpanelGroup } from "./gdpr-utils";
+import { apiActions } from "./api-actions";
+import { _ } from "./utils";
 
 /**
  * Mixpanel Group Object
  * @constructor
  */
-var MixpanelGroup = function() {};
+var MixpanelGroup = function () {};
 
 _.extend(MixpanelGroup.prototype, apiActions);
 
-MixpanelGroup.prototype._init = function(mixpanel_instance, group_key, group_id) {
-    this._mixpanel = mixpanel_instance;
-    this._group_key = group_key;
-    this._group_id = group_id;
+MixpanelGroup.prototype._init = function (
+  mixpanel_instance,
+  group_key,
+  group_id
+) {
+  this._mixpanel = mixpanel_instance;
+  this._group_key = group_key;
+  this._group_id = group_id;
 };
 
 /**
@@ -35,12 +39,16 @@ MixpanelGroup.prototype._init = function(mixpanel_instance, group_key, group_id)
  * @param {*} [to] A value to set on the given property name
  * @param {Function} [callback] If provided, the callback will be called after the tracking event
  */
-MixpanelGroup.prototype.set = addOptOutCheckMixpanelGroup(function(prop, to, callback) {
-    var data = this.set_action(prop, to);
-    if (_.isObject(prop)) {
-        callback = to;
-    }
-    return this._send_request(data, callback);
+MixpanelGroup.prototype.set = addOptOutCheckMixpanelGroup(function (
+  prop,
+  to,
+  callback
+) {
+  var data = this.set_action(prop, to);
+  if (_.isObject(prop)) {
+    callback = to;
+  }
+  return this._send_request(data, callback);
 });
 
 /**
@@ -63,12 +71,16 @@ MixpanelGroup.prototype.set = addOptOutCheckMixpanelGroup(function(prop, to, cal
  * @param {*} [to] A value to set on the given property name
  * @param {Function} [callback] If provided, the callback will be called after the tracking event
  */
-MixpanelGroup.prototype.set_once = addOptOutCheckMixpanelGroup(function(prop, to, callback) {
-    var data = this.set_once_action(prop, to);
-    if (_.isObject(prop)) {
-        callback = to;
-    }
-    return this._send_request(data, callback);
+MixpanelGroup.prototype.set_once = addOptOutCheckMixpanelGroup(function (
+  prop,
+  to,
+  callback
+) {
+  var data = this.set_once_action(prop, to);
+  if (_.isObject(prop)) {
+    callback = to;
+  }
+  return this._send_request(data, callback);
 });
 
 /**
@@ -81,9 +93,12 @@ MixpanelGroup.prototype.set_once = addOptOutCheckMixpanelGroup(function(prop, to
  * @param {String} prop The name of the property.
  * @param {Function} [callback] If provided, the callback will be called after the tracking event
  */
-MixpanelGroup.prototype.unset = addOptOutCheckMixpanelGroup(function(prop, callback) {
-    var data = this.unset_action(prop);
-    return this._send_request(data, callback);
+MixpanelGroup.prototype.unset = addOptOutCheckMixpanelGroup(function (
+  prop,
+  callback
+) {
+  var data = this.unset_action(prop);
+  return this._send_request(data, callback);
 });
 
 /**
@@ -98,12 +113,16 @@ MixpanelGroup.prototype.unset = addOptOutCheckMixpanelGroup(function(prop, callb
  * @param {Array} values Values to merge with the given property
  * @param {Function} [callback] If provided, the callback will be called after the tracking event
  */
-MixpanelGroup.prototype.union = addOptOutCheckMixpanelGroup(function(list_name, values, callback) {
-    if (_.isObject(list_name)) {
-        callback = values;
-    }
-    var data = this.union_action(list_name, values);
-    return this._send_request(data, callback);
+MixpanelGroup.prototype.union = addOptOutCheckMixpanelGroup(function (
+  list_name,
+  values,
+  callback
+) {
+  if (_.isObject(list_name)) {
+    callback = values;
+  }
+  var data = this.union_action(list_name, values);
+  return this._send_request(data, callback);
 });
 
 /**
@@ -115,10 +134,12 @@ MixpanelGroup.prototype.union = addOptOutCheckMixpanelGroup(function(list_name, 
  *
  * @param {Function} [callback] If provided, the callback will be called after the tracking event
  */
-MixpanelGroup.prototype['delete'] = addOptOutCheckMixpanelGroup(function(callback) {
-    // bracket notation above prevents a minification error related to reserved words
-    var data = this.delete_action();
-    return this._send_request(data, callback);
+MixpanelGroup.prototype["delete"] = addOptOutCheckMixpanelGroup(function (
+  callback
+) {
+  // bracket notation above prevents a minification error related to reserved words
+  var data = this.delete_action();
+  return this._send_request(data, callback);
 });
 
 /**
@@ -132,43 +153,61 @@ MixpanelGroup.prototype['delete'] = addOptOutCheckMixpanelGroup(function(callbac
  * @param {Object} value Value to remove from the given group property
  * @param {Function} [callback] If provided, the callback will be called after the tracking event
  */
-MixpanelGroup.prototype.remove = addOptOutCheckMixpanelGroup(function(list_name, value, callback) {
-    var data = this.remove_action(list_name, value);
-    return this._send_request(data, callback);
+MixpanelGroup.prototype.remove = addOptOutCheckMixpanelGroup(function (
+  list_name,
+  value,
+  callback
+) {
+  var data = this.remove_action(list_name, value);
+  return this._send_request(data, callback);
 });
 
-MixpanelGroup.prototype._send_request = function(data, callback) {
-    data['$group_key'] = this._group_key;
-    data['$group_id'] = this._group_id;
-    data['$token'] = this._get_config('token');
+MixpanelGroup.prototype._send_request = function (data, callback) {
+  data["$group_key"] = this._group_key;
+  data["$group_id"] = this._group_id;
+  data["$token"] = this._get_config("token");
 
-    var date_encoded_data = _.encodeDates(data);
-    return this._mixpanel._track_or_batch({
-        type: 'groups',
-        data: date_encoded_data,
-        endpoint: this._get_config('api_host') + '/' +  this._get_config('api_routes')['groups'],
-        batcher: this._mixpanel.request_batchers.groups
-    }, callback);
+  var date_encoded_data = _.encodeDates(data);
+
+  // Get api_host for groups endpoint if available, otherwise use default api_host
+  var api_host =
+    this._get_config("api_hosts")["groups"] || this._get_config("api_host");
+
+  return this._mixpanel._track_or_batch(
+    {
+      type: "groups",
+      data: date_encoded_data,
+      endpoint: api_host + "/" + this._get_config("api_routes")["groups"],
+      batcher: this._mixpanel.request_batchers.groups,
+    },
+    callback
+  );
 };
 
-MixpanelGroup.prototype._is_reserved_property = function(prop) {
-    return prop === '$group_key' || prop === '$group_id';
+MixpanelGroup.prototype._is_reserved_property = function (prop) {
+  return prop === "$group_key" || prop === "$group_id";
 };
 
-MixpanelGroup.prototype._get_config = function(conf) {
-    return this._mixpanel.get_config(conf);
+MixpanelGroup.prototype._get_config = function (conf) {
+  return this._mixpanel.get_config(conf);
 };
 
-MixpanelGroup.prototype.toString = function() {
-    return this._mixpanel.toString() + '.group.' + this._group_key + '.' + this._group_id;
+MixpanelGroup.prototype.toString = function () {
+  return (
+    this._mixpanel.toString() +
+    ".group." +
+    this._group_key +
+    "." +
+    this._group_id
+  );
 };
 
 // MixpanelGroup Exports
-MixpanelGroup.prototype['remove']   = MixpanelGroup.prototype.remove;
-MixpanelGroup.prototype['set']      = MixpanelGroup.prototype.set;
-MixpanelGroup.prototype['set_once'] = MixpanelGroup.prototype.set_once;
-MixpanelGroup.prototype['union']    = MixpanelGroup.prototype.union;
-MixpanelGroup.prototype['unset']    = MixpanelGroup.prototype.unset;
-MixpanelGroup.prototype['toString'] = MixpanelGroup.prototype.toString;
+MixpanelGroup.prototype["remove"] = MixpanelGroup.prototype.remove;
+MixpanelGroup.prototype["set"] = MixpanelGroup.prototype.set;
+MixpanelGroup.prototype["set_once"] = MixpanelGroup.prototype.set_once;
+MixpanelGroup.prototype["union"] = MixpanelGroup.prototype.union;
+MixpanelGroup.prototype["unset"] = MixpanelGroup.prototype.unset;
+MixpanelGroup.prototype["toString"] = MixpanelGroup.prototype.toString;
 
-export {MixpanelGroup};
+export { MixpanelGroup };
